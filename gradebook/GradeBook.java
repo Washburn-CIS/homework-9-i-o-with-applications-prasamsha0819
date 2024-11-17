6e296e8062926bd9ebe685179915176aa76771ea
prasamsha0819 <prasamshasingh.thakuri@washburn.edu>
2024-11-17 06:15:27 +0000

    tried

--- gradebook/GradeBook.java
+++ gradebook/GradeBook.java
@@ -1,12 +1,34 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
 public class GradeBook {
     private static Student[] students;
+    private static int studentCount = 0; 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+        throws FileNotFoundException {
         Scanner input = new Scanner(System.in);
 
         // TODO: initialize students from contents of grades.txt file
+        students = new Student[100];
+        File gradesFile = new File("grades.txt");
+        Scanner gradeScanner = new Scanner(gradesFile); // thiis will read from the file
+
+        while (gradeScanner.hasNextLine()){
+            String line = gradeScanner.nextLine();
+            String[] tokens = line.split(",");
+            if (tokens.length == 3){
+            String lastName = tokens[0];
+            String firstName = tokens[1];
+            double grade = Double.parseDouble(tokens[2]); 
+            students[studentCount++] = new Student(firstName, lastName, grade); 
+
+            }
+        }
+        
+        gradeScanner.close(); 
 
         System.out.println("Welcome to the CM111 Grade Book App!");
 
@@ -21,8 +43,8 @@ public class GradeBook {
             switch(choice) {
                 case "1": 
                     for(Student student: students) {
-                        System.out.printf("%s, %s: %f%n", student.getFirstName(), 
-                                                        student.getLastName(), 
+                        System.out.printf("%s, %s: %f%n", student.getLastName(), 
+                                                        student.getFirstName(), 
                                                         student.getGrade());
                     }
                     break;
@@ -45,8 +67,18 @@ public class GradeBook {
                     break;
                 case "3":
                     // Challenge: write code to save the grades to grades.txt
-                    System.out.println("Goodbye!");
+                    PrintWriter writer = new PrintWriter("grades.txt");
+                    for (Student student : students) {
+                        writer.printf("%s,%s,%.2f%n", 
+                            student.getLastName(), 
+                            student.getFirstName(), 
+                            student.getGrade()); 
+                    }
+                    writer.close(); 
+                    System.out.println("Grades saved. Goodbye!");
                     return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
 
             }
         }
